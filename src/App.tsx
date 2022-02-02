9be083d697076a4149e17616e077af9654873d76
mathers08 <black.mathers08@mail.ru>
2022-02-03 00:10:53 +0300

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import {Route, Routes,} from 'react-router-dom';
 import './App.scss';
-import {UserProfile, UsersList} from "./components";
-import {Route, Routes} from "react-router-dom";
-import {IUser} from "./types/user";
-import axios from "axios";
+import {UserProfile, UsersList} from './components';
+import SortBlock from './components/UsersList/SortBlock';
+import {IUser} from './types/user';
 
 const App = () => {
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchedItems = async () => {
+      const {data: users} = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setUsers(users);
+      setIsLoading(false);
+    }
+    fetchedItems();
+  }, [])
+
+  const sortUsersByAddress = () => {
+    setUsers([...users].sort((a, b) => (a.address.city > b.address.city) ? 1 : -1));
+  }
+
+  const sortUsersByCompany = () => {
+    setUsers([...users].sort((a, b) => (a.company!.name > b.company!.name) ? 1 : -1));
+  }
+
   return (
-    <Routes>
-      <Route path='/' element={<UsersList />}/>
-      <Route path='/profile/:id' element={<UserProfile />}/>
-    </Routes>
-  );
+    <div className='container'>
+      <SortBlock sortUsersByAddress={sortUsersByAddress} sortUsersByCompany={sortUsersByCompany}/>
+      <Routes>
+        <Route path="/" element={<UsersList users={users} isLoading={isLoading}/>}/>
+        <Route path="/profile/:id" element={<UserProfile/>}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
